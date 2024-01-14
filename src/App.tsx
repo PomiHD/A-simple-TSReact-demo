cba2c7f65a479d0df8113170cd33c44b93b52fd4
PomiHD <wsgddjy@live.com>
2024-01-15 00:03:57 +1000

    add 4th button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const content = [
+    [
+        "React is extremely popular",
+        "It makes building complex, interactive UIs a breeze",
+        "It's powerful & flexible",
+        "It has a very active and versatile ecosystem"
+    ],
+    [
+        "Components, JSX & Props",
+        "State",
+        "Hooks (e.g., useEffect())",
+        "Dynamic rendering"
+    ],
+    [
+        "Official web page (react.dev)",
+        "Next.js (Fullstack framework)",
+        "React Native (build native mobile apps with React)"
+    ],
+    [
+        "hello",
+        "aaaaaah",
+        "yeeee"
+    ]
+];
+
+export default function App() {
+    const [activeContentIndex, setActiveContentIndex] = useState(0);
+
+    return (
+        <div>
+            <header>
+                <img src="react-logo-xs.png" alt="React logo" />
+                <div>
+                    <h1>React.js</h1>
+                    <p>i.e., using the React library for rendering the UI</p>
+                </div>
+            </header>
+
+            <div id="tabs">
+                <menu>
+                    <button
+                        className={activeContentIndex === 0 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(0)}
+                    >
+                        Why React?
+                    </button>
+                    <button
+                        className={activeContentIndex === 1 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(1)}
+                    >
+                        Core Features
+                    </button>
+                    <button
+                        className={activeContentIndex === 2 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(2)}
+                    >
+                        Related Resources
+                    </button>
+
+                    <button
+                        className={activeContentIndex === 3 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(3)}
+                    >
+                        Extra Resources
+                    </button>
+                    
+                </menu>
+                <div id="tab-content">
+                    <ul>
+                        {content[activeContentIndex].map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
