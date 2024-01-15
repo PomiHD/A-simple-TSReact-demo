ccffb2c8f95f1746d13fcfd360860fbd1d8c1ca2
PomiHD <wsgddjy@live.com>
2024-01-15 16:13:10 +1000

    1/15/2024 interact with react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,31 @@
-import { useState } from "react";
-import "./styles.css";
+import img1 from  "./assets/react-core-concepts.png"
 
-const content = [
-    [
-        "React is extremely popular",
-        "It makes building complex, interactive UIs a breeze",
-        "It's powerful & flexible",
-        "It has a very active and versatile ecosystem"
-    ],
-    [
-        "Components, JSX & Props",
-        "State",
-        "Hooks (e.g., useEffect())",
-        "Dynamic rendering"
-    ],
-    [
-        "Official web page (react.dev)",
-        "Next.js (Fullstack framework)",
-        "React Native (build native mobile apps with React)"
-    ],
-    [
-        "hello",
-        "aaaaaah",
-        "yeeee"
-    ]
-];
+import React from "react";
 
-export default function App() {
-    const [activeContentIndex, setActiveContentIndex] = useState(0);
-
+function Header() {
     return (
         <div>
             <header>
-                <img src="react-logo-xs.png" alt="React logo" />
-                <div>
-                    <h1>React.js</h1>
-                    <p>i.e., using the React library for rendering the UI</p>
-                </div>
+                <img src={img1} alt="Stylized atom" />
+                <h1>React Essentials</h1>
+                <p>
+                    Fundamental React concepts you will need for almost any app you are
+                    going to build!
+                </p>
             </header>
-
-            <div id="tabs">
-                <menu>
-                    <button
-                        className={activeContentIndex === 0 ? "active" : ""}
-                        onClick={() => setActiveContentIndex(0)}
-                    >
-                        Why React?
-                    </button>
-                    <button
-                        className={activeContentIndex === 1 ? "active" : ""}
-                        onClick={() => setActiveContentIndex(1)}
-                    >
-                        Core Features
-                    </button>
-                    <button
-                        className={activeContentIndex === 2 ? "active" : ""}
-                        onClick={() => setActiveContentIndex(2)}
-                    >
-                        Related Resources
-                    </button>
-
-                    <button
-                        className={activeContentIndex === 3 ? "active" : ""}
-                        onClick={() => setActiveContentIndex(3)}
-                    >
-                        Extra Resources
-                    </button>
-                    
-                </menu>
-                <div id="tab-content">
-                    <ul>
-                        {content[activeContentIndex].map((item) => (
-                            <li key={item}>{item}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
         </div>
     );
 }
+
+function App() {
+    return (
+        <div>
+            <Header></Header>
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
+}
+
+export default App;
