39475aaa0b7cc9c253764e9ee01e7932e9746abc
PomiHD <wsgddjy@live.com>
2024-01-16 01:01:10 +1000

    1/16/2024 dynamically output contents & load image file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import img1 from  "./assets/react-core-concepts.png"
-
+import img1 from "./assets/react-core-concepts.png"
+import component from "./assets/components.png"
 import React from "react";
+console.log(typeof component)
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 
 function Header() {
+    const description = reactDescriptions[genRandomInt(2)];
+
     return (
         <div>
             <header>
-                <img src={img1} alt="Stylized atom" />
+                {/*better practice: use import to refer to the image file*/}
+                <img src={img1} alt="Stylized atom"/>
                 <h1>React Essentials</h1>
                 <p>
-                    Fundamental React concepts you will need for almost any app you are
+                    {description} React concepts you will need for almost any app you are
                     going to build!
                 </p>
             </header>
@@ -17,12 +28,43 @@ function Header() {
     );
 }
 
+function CoreConcept({title, description, image }: { title: string; description: string ; image:string }) {
+    return (
+        <li>
+            <img src={image} alt={title}/>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+
+    )
+}
+
 function App() {
     return (
         <div>
-            <Header></Header>
+            {/*<Header></Header>*/}
+            {/*<Header />*/}
+            <Header/>
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept 
+                            title="Compoent 1" 
+                            description="UI 1"
+                            image = {component}/>
+                        <CoreConcept
+                            title="Compoent 2"
+                            description="UI 2"
+                            image = {component}/>
+                        <CoreConcept
+                            title="Compoent 3"
+                            description="UI 3"
+                            image = {component}/>
+               </ul>
+                </section>
+
+
             </main>
         </div>
     );
