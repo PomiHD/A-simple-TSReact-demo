fb0c7af0fa4973cdeb6cbe8c5020660b5e1de880
PomiHD <wsgddjy@live.com>
2024-01-16 22:50:29 +1000

    a leaner way to show tab contents

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,22 @@ function App() {
     }
 
     console.log("App component executing.")
-    
+    let tabContent = <p>Please select a topic.</p>;
+    if(selectedTopic){
+        tabContent = (<div id="tab-content">
+            <h3>
+                {EXAMPLES[selectedTopic].title}
+            </h3>
+            <p>
+                {EXAMPLES[selectedTopic].description}
+            </p>
+            <pre>
+                            <code>
+                                {EXAMPLES[selectedTopic].code}
+                            </code>
+                        </pre>
+        </div>)
+    }
     return (
         <div>
             {/*<Header></Header>*/}
@@ -86,19 +101,7 @@ function App() {
                             State
                         </TabButton>
                     </menu>
-                    {!selectedTopic ? <p>Please select a topic.</p> : (<div id="tab-content">
-                        <h3>
-                            {EXAMPLES[selectedTopic].title}
-                        </h3>
-                        <p>
-                            {EXAMPLES[selectedTopic].description}
-                        </p>
-                        <pre>
-                            <code>
-                                {EXAMPLES[selectedTopic].code}
-                            </code>
-                        </pre>
-                    </div>)}
+                    {tabContent}
                 </section>
             </main>
         </div>
