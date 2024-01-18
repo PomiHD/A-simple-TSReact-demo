7d22648599882cbaed816cc06d834ee3c92bf1de
PomiHD <wsgddjy@live.com>
2024-01-18 15:50:15 +1000

    66. Working with Multiple JSX Slots

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { EXAMPLES } from "../data-with-examples";
 import Section from "./Section";
+import Tab from "./Tab";
 // Define the type for the EXAMPLES keys, if it's not already defined
 type ExampleKey = keyof typeof EXAMPLES;
 export default function Examples() {
@@ -32,40 +33,45 @@ export default function Examples() {
   }
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        {/*Note here we use "()=>handelSelect("Components")".If only use "handelSelect("Components")", then*/}
-        {/*The issue with code is in the way the handelSelect function is being called */}
-        {/*on the onSelect event of the button. In its current state, */}
-        {/*the function is being executed immediately when the component is rendered,*/}
-        {/*not when the button is clicked. This causes the function to be called continuously */}
-        {/*in an infinite loop,*/}
-        {/*which is likely crashing the application.*/}
-        <TabButton
-          isSelected={selectedTopic == "components"}
-          onClick={() => handelSelect("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic == "jsx"}
-          onClick={() => handelSelect("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic == "props"}
-          onClick={() => handelSelect("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic == "state"}
-          onClick={() => handelSelect("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <Tab
+        buttons={
+          <>
+            {/*Note here we use "()=>handelSelect("Components")".If only use "handelSelect("Components")", then*/}
+            {/*The issue with code is in the way the handelSelect function is being called */}
+            {/*on the onSelect event of the button. In its current state, */}
+            {/*the function is being executed immediately when the component is rendered,*/}
+            {/*not when the button is clicked. This causes the function to be called continuously */}
+            {/*in an infinite loop,*/}
+            {/*which is likely crashing the application.*/}
+            <TabButton
+              isSelected={selectedTopic == "components"}
+              onClick={() => handelSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "jsx"}
+              onClick={() => handelSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "props"}
+              onClick={() => handelSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "state"}
+              onClick={() => handelSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tab>
     </Section>
   );
 }
---
+++ src/components/Tab.tsx
@@ -0,0 +1,8 @@
+﻿export default function Tab({ children, buttons }) {
+  return (
+    <>
+      <menu>{buttons}</menu>
+      {children}
+    </>
+  );
+}
